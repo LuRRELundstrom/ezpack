1fe0b06b5754127d521fd7158c3719404a734774
Chris Lundstrom <lulleh_@hotmail.com>
2017-11-18 12:15:07 +0100

    Init commit

---
+++ SimplePack/FileOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class FileOperations
+{
+
+    public void AwaitInput(string[] args)
+    {
+
+        if (args.Length > 0 && File.Exists(args[0]))
+        {
+            string path = args[0];
+            foreach (var item in args)
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string _parentfolder = Directory.GetParent(item).Name;
+                    Console.WriteLine(_parentfolder);
+
+                }
+            }
+            Console.ReadKey();
+        }
+
+        else
+        {
+            Console.WriteLine("Awaiting input..");
+        }
+    }
+
+}
---
+++ SimplePack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePack
+{
+    class Program
+    {
+       /*
+       
+
+
+
+      */
+
+        static void Main(string[] args)
+        {
+            FileOperations await = new FileOperations();
+            await.AwaitInput(args);
+
+        }
+
+
+        }
+    }
