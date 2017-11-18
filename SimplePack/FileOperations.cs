5b1a8d4ad3b6d43c8a6fe47436eb3ea60c17e6a7
Chris Lundstrom <lulleh_@hotmail.com>
2017-11-18 17:42:37 +0100

    added support for mutiple .zip unpacking

--- SimplePack/FileOperations.cs
+++ SimplePack/FileOperations.cs
@@ -1,31 +1,117 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
 
-class FileOperations
+
+/*
+    AUTHOR: LuRRE
+
+
+    DESCRIPTION:
+        Drag and drop items onto .exe to print parent folder name or await the drag and drop onto the console window ASYNC.
+
+
+    PARAMETERS:
+        ARGS[] = The string names of files started with APP.
+        ParentFolder = " "
+        FileName
+        FullPath
+
+    RETURN:
+
+*/
+namespace SimplePack
 {
 
-    public void AwaitInput(string[] args)
+    class FileOperations
     {
+        public string ParentFolder { get; set; }
+        public string FileName { get; set; }
 
-        if (args.Length > 0 && File.Exists(args[0]))
+        public string FullPath { get; set; }
+
+        public string InitialInput(string[] args)
         {
-            string path = args[0];
-            foreach (var item in args)
+            if (args.Length > 0 && File.Exists(args[0]))
             {
-                using (StreamReader sr = new StreamReader(path))
-                {
-                    string _parentfolder = Directory.GetParent(item).Name;
-                    Console.WriteLine(_parentfolder);
 
+                foreach (var item in args)
+                {
+                    string path = args[0];
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+
+                        //Prints parent folder of file, later used for renaming
+                        string parentfolder = Directory.GetParent(item).Name;
+                        string filename = Path.GetFileName(item);
+                        string fullpath = Path.GetFullPath(item);
+                        Console.WriteLine("Extracting {1} to {0}", parentfolder, filename);
+                        Console.WriteLine("Extension: {0}\n", Path.GetExtension(item));
+                        //Saving strings to respective properties
+                        FullPath = fullpath;
+                        FileName = filename;
+                        ParentFolder = parentfolder;
+                    }
+                }
+
+
+                return ParentFolder;
+            }
+            Console.WriteLine("No input found.\nDrag and drop files onto ezpack.exe");
+            Console.ReadKey();
+            return null;
+        }
+
+        public void Unrar(string[] args)
+        {
+            int extracted = 0;
+            try
+            {
+                CheckArgs(args);
+
+                foreach (var item in args)
+                {
+                    
+                    string path = args[0];
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        //Extract zip from item location to parent folder.
+                        ZipFile.ExtractToDirectory(item, ParentFolder);
+                        Console.WriteLine("Unpacking: {0}", FullPath);
+                        extracted++;
+                    }
                 }
             }
-            Console.ReadKey();
+            catch (Exception e)
+            {
+                Console.WriteLine("File already exists\n");
+                
+            }
+
+            finally
+            {
+                
+            
+            Console.WriteLine("{0} archive(s) extracted.", extracted);
+            Console.ReadLine();
+            }
         }
 
-        else
+        public bool CheckArgs(string[] args)
         {
-            Console.WriteLine("Awaiting input..");
-        }
-    }
+            foreach (var item in args)
+            {
+                if (Path.GetExtension(item) != ".zip")
+                {
+                    Console.WriteLine("File extension must be .zip");
+                    return false;
+                }
+            }
 
+            return true;
+        }
+
+    }
 }
--- SimplePack/Program.cs
+++ SimplePack/Program.cs
@@ -1,26 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+	AUTHOR
+
+
+	DESCRIPTION
+
+
+
+    PARAMETERS
+
+
+    RETURN
+
+*/
 namespace SimplePack
 {
     class Program
     {
-       /*
-       
 
-
-
-      */
 
         static void Main(string[] args)
         {
-            FileOperations await = new FileOperations();
-            await.AwaitInput(args);
+            Console.WriteLine("EZpack by LuRRE\n");
+            FileOperations fo = new FileOperations();
+            string init = fo.InitialInput(args);
+            if (init != null) fo.Unrar(args);
 
         }
 
 
-        }
     }
+}
